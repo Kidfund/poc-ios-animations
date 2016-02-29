2fc63e94c54da795a4608a4d7759fa7eeec378a8
Tim Broder <timothy.broder@gmail.com>
2016-02-29 14:22:38 -0500

    Initial Commit

---
+++ KidfundPoc/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  KidfundPoc
+//
+//  Created by Tim Broder on 2/29/16.
+//  Copyright © 2016 Kidfund. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
